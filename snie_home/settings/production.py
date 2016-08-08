c42cf00fc3cb13d93bbab8313d842e32b6a5ea00
snie <snie@ncsu.edu>
2016-08-07 23:45:14 -0400

    Use production.py in heroku

--- snie_home/settings/production.py
+++ snie_home/settings/production.py
@@ -1,8 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['poetry-ncsu.herokuapp.com']
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = ['snie-home.herokuapp.com']
 
 DEBUG = False
