2d17ce64635e281ea0f02a1c1bc3c40bbab106dd
snie <snie@ncsu.edu>
2016-08-07 22:50:59 -0400

    Set without database

--- snie_home/settings/base.py
+++ snie_home/settings/base.py
@@ -79,16 +79,6 @@ MIDDLEWARE_CLASSES = (
 
 # Configure database
 import dj_database_url
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mydatabase',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # DATABASES = {}
 # DATABASES['default'] =  dj_database_url.config()
