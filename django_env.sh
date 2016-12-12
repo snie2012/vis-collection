2195f018df3ef1109a5f87a03c847c36ae716b84
snie <snie@ncsu.edu>
2016-12-13 07:48:59 +0800

    update

---
+++ django_env.sh
@@ -0,0 +1,5 @@
+export DJANGO_SETTINGS_MODULE="snie_collection.settings.development"
+
+export DATABASE_URL="postgres://django:django@172.17.0.2/django_db"
+
+export SECRET_KEY="u7%$+slp@u&4)3k97+jse#v*%=azgi5j%i94_n877kk-3z#wsz"
